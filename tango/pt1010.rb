fd1bbbfdf73db4bc86b0042f61fa7a0102414db8
Anton Maurovic <anton.maurovic@gmail.com>
2014-04-02 23:15:35 +1030

    Tango: Added Pin 4 signal to a secondary PT-1010 timing diagram example

--- tango/pt1010.rb
+++ tango/pt1010.rb
@@ -98,7 +98,69 @@ t = Tango::Scope.new do
   end
 end
 
-t.write_svg('pt1010-CLK-DATA.svg', fold: (:bit_loop_begin..:bit_loop_end))
+f = 'pt1010-CLK-DATA.svg'
+puts f
+t.write_svg(f, fold: (:bit_loop_begin..:bit_loop_end))
 
 
+# Adding in pin 4:
+t = Tango::Scope.new do
+  yellow_with_rip
+  fold_type type: :saw2, gap_color: '#eec', teeth: 10, gap_width: 15, gap_corner: 'miter'
+  height 330
+  channel :P4, initial: false, color: '#f90', subtext: '(Pin 4)', negative: true
+  repeat(2, 'Main Cycle', period: 14400) do |line|
+    mark :cycle_start, hide: true
+    label("START\nCYCLE ##{line+1}")
+    end_measure('Inactive time')
+    end_measure('Main Cycle')
+    start_measure('Main Cycle', y: -1.5, align: %w(left right), units: :ms)
+    measure('Total CLK activity', y: -1.0, align: :left) do
+      measure('CLK "front porch"', y: -0.5) do
+        sample 0..9, CLK: true
+      end
+      repeat(8, 'Bytes Loop') do |byte|
+        label "Byte #{7-byte}"
+        repeat(8, 'Bit Loop') do |bit|
+          measure('CLK cycle', y: 0.5) do
+            sample 0..1, CLK: false, DATA: "#{63-byte*8-bit}"
+            start_measure('CLK high between bytes', y: -0.5) if bit==7
+            if bit==7 && byte==7
+              start_measure('CLK "back porch"', y: -0.5)
+              mark :loop_end, hide: true
+            elsif bit==2 && byte==1
+              fold :bit_loop_begin
+            elsif bit==2 && byte==7
+              fold :bit_loop_end
+            end
+            sample 0..1, CLK: true
+          end
+        end
+        label 'End byte 0' if byte==7
+        measure('Last bit extra hold time', y: 0.5, outer: true) do
+          sample 1, DATA: true
+        end
+        step 6.75
+        end_measure('CLK high between bytes')
+      end
+      mark_seek :loop_end, 11.25
+      sample 0, CLK: false
+      end_measure('CLK "back porch"')
+    end # Measure: Total active time.
+    measure('Gap before pin 4 pulse', y: 0.5) do
+      sample 8.125, P4: true
+    end
+    measure('Pin 4 pulse width', y: 1.5) do
+      sample 1.875, P4: false
+    end
+    start_measure('Inactive time', y: -0.5, align: :left, override: '~ 14.2ms')
+    step 6
+    fold :bit_loop_begin
+    mark_seek :cycle_start, 14397 # Seek to 4us before the end of this cycle.
+    fold :bit_loop_end
+  end
+end
 
+f = 'pt1010-LATCH.svg'
+puts f
+t.write_svg(f, fold: (:bit_loop_begin..:bit_loop_end))
