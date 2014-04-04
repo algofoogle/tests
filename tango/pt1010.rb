82406c8c0fbe96aedd2338284aa05d33f8d86f9f
Anton Maurovic <anton.maurovic@gmail.com>
2014-04-04 20:22:40 +1030

    Tango: Adding PT-1010 chart with STROBE

--- tango/pt1010.rb
+++ tango/pt1010.rb
@@ -31,7 +31,7 @@ def yellow_with_rip
   style(
     measures: { stroke: '#999', stroke_width: 0.4 },
     label_lines: { stroke: 'cyan', stroke_width: 0.5, stroke_dasharray: '5,5' },
-    waveform_base: { stroke_width: 1.5 },
+    waveform_base: { stroke_width: 1.8 },
     fold_band: { stroke: 'none', fill: background_color },
     fold_edge: { stroke_dasharray: nil, fill: 'none', stroke: '#fff', stroke_width: 12, stroke_linecap: 'round' }
   )
@@ -44,7 +44,7 @@ def white_with_zigzag
   style(
     measures: { stroke: '#999', stroke_width: 0.4 },
     label_lines: { stroke: 'cyan', stroke_width: 0.5, stroke_dasharray: '5,5' },
-    waveform_base: { stroke_width: 1.5 },
+    waveform_base: { stroke_width: 1.8 },
     fold_edge: { stroke_dasharray: nil, fill: 'none', stroke: '#aaa', stroke_width: 4, stroke_linecap: 'round' }
   )
 end
@@ -162,5 +162,83 @@ t = Tango::Scope.new do
 end
 
 f = 'pt1010-LATCH.svg'
+puts f
+t.write_svg(f, fold: (:bit_loop_begin..:bit_loop_end))
+
+
+
+# Adding in pin 8:
+t = Tango::Scope.new do
+  yellow_with_rip
+  fold_type type: :saw2, gap_color: '#eec', teeth: 12, gap_width: 15, gap_corner: 'miter'
+  width 1100
+  height 400
+  channel :P4, initial: false, color: '#f90', subtext: '(Pin 4)', negative: true
+  channel :P8, initial: true, color: '#e00', subtext: '(Pin 8)'
+  repeat(2, 'Main Cycle', period: 14400) do |line|
+    mark :cycle_start, hide: true
+    label("START\nCYCLE ##{line+1}")
+    end_measure('Inactive time')
+    end_measure('Main Cycle')
+    start_measure('Main Cycle', y: -1.5, align: %w(left right), units: :ms)
+    measure('Total CLK activity', y: -1.0, align: :left) do
+      measure('CLK "front porch"', y: -0.5) do
+        sample 0..9, CLK: true
+      end
+      repeat(8, 'Bytes Loop') do |byte|
+        label "Byte #{7-byte}"
+        repeat(8, 'Bit Loop') do |bit|
+          measure('CLK cycle', y: 0.5) do
+            sample 0..1, CLK: false, DATA: "#{63-byte*8-bit}"
+            start_measure('CLK high between bytes', y: -0.5) if bit==7
+            if bit==7 && byte==7
+              start_measure('CLK "back porch"', y: -0.5)
+              mark :loop_end, hide: true
+            elsif bit==2 && byte==1
+              fold :bit_loop_begin
+            elsif bit==2 && byte==7
+              fold :bit_loop_end
+            end
+            sample 0..1, CLK: true
+          end
+        end
+        label 'End byte 0' if byte==7
+        measure('Last bit extra hold time', y: 0.5, outer: true) do
+          sample 1, DATA: true
+        end
+        step 6.75
+        end_measure('CLK high between bytes')
+      end
+      mark_seek :loop_end, 11.25
+      sample 0, CLK: false
+      end_measure('CLK "back porch"')
+    end # Measure: Total active time.
+    measure('Gap before pin 4 pulse', y: 0.5) do
+      sample 8.125, P4: true
+    end
+    measure('Pin 4 pulse width', y: 1.5) do
+      sample 1.875, P4: false
+    end
+    measure('Gap before pin 8 falls', y: 2.5) do
+      sample 2.375, P8: false
+    end
+    measure('Pin 8 low time', y: 3.5, override: '5.68ms') do
+      mark :pin8_low_start, hide: true
+      step 8
+      fold :bit_loop_begin
+      mark_seek :pin8_low_start, 5676
+      fold :bit_loop_end
+      mark_seek :pin8_low_start, 5680
+      sample 0, P8: true
+      start_measure('Inactive time', y: 2.5, align: :right, override: '~ 8.5ms')
+    end
+    step 10
+    fold :bit_loop_begin
+    mark_seek :cycle_start, 14395 # Seek to 4us before the end of this cycle.
+    fold :bit_loop_end
+  end
+end
+
+f = 'pt1010-STROBE.svg'
 puts f
 t.write_svg(f, fold: (:bit_loop_begin..:bit_loop_end))
