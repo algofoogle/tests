d3102ed0ea04d5e3007e1eb58673cac0ee41017a
algofoogle <anton@foogle.com>
2016-10-17 23:13:57 +1030

    Fix to HTTP 404 error for NodeMCU

--- nodemcu/01/web.lua
+++ nodemcu/01/web.lua
@@ -1,6 +1,6 @@
 function http_404(sck)
 	sck:send(
-		"HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n",
+		"HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n" ..
 		"<html><body><h1>404 Not Found</h1><p>Sorry.</p></body></html>"
 	)
 end
