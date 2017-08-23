29015115886b9446ade0bd9adbffb14c5405efcc
Anton Maurovic <anton@catapult-elearning.com>
2017-08-23 14:04:12 +0930

    Improving RFB test flow

--- rfb/main.c
+++ rfb/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,7 +9,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define PORT 5900
+#define PORT 5905
 
 #define MAXPENDING 5
 
@@ -17,18 +18,42 @@
 #define U8 unsigned char
 #define U16 unsigned short
 #define U32 unsigned int
+#define S32 int
 
 #define BUILD_BUG_ON(condition) extern char _BUILD_BUG_ON_ [ sizeof(char[1 - 2*!!(condition)]) ]
+
+#define Default(zzsrc,zzalt) ((zzsrc) ? (zzsrc) : (zzalt))
 
 BUILD_BUG_ON(sizeof(U8) != 1);
 BUILD_BUG_ON(sizeof(U16) != 2);
 BUILD_BUG_ON(sizeof(U32) != 4);
+BUILD_BUG_ON(sizeof(S32) != 4);
 
 enum {
   RFB_SEC_INVALID = 0,
   RFB_SEC_NONE = 1,
   RFB_SEC_VNC = 2,
 };
+
+
+// This is used to tell GCC that we want our structs packed exactly
+// as stated with no automatic padding/alignment:
+#define PACKED __attribute__((packed))
+
+#define B0(zzs) ((zzs)&0xFFL)
+#define B1(zzs) ((zzs>>8)&0xFFL)
+#define B2(zzs) ((zzs>>16)&0xFFL)
+#define B3(zzs) ((zzs>>24)&0xFFL)
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define RFB8(zzs) (zzs)
+#define RFB16(zzs) ((B0(zzs)<<8) | (B1(zzs)))
+#define RFB32(zzs) ((B0(zzs)<<24) | (B1(zzs)<<16) | (B2(zzs)<<8) | (B3(zzs)))
+#else
+#define RFB8(zzs) (zzs)
+#define RFB16(zzs) ((zzs)&0xFFFFL)
+#define RFB32(zzs) ((zzs)&0xFFFFFFFFL)
+#endif
 
 
 typedef struct {
@@ -43,7 +68,55 @@ typedef struct {
   U8 g_shift;
   U8 b_shift;
   U8 _padding[3];
-} pixel_format;
+} PACKED pixel_format;
+
+BUILD_BUG_ON(sizeof(pixel_format) != 16);
+
+
+typedef struct {
+  U8 _padding[3];
+  pixel_format format;
+} PACKED SetPixelFormat_t;
+
+BUILD_BUG_ON(sizeof(SetPixelFormat_t) != 19);
+
+// Variable length:
+typedef struct {
+  U8 _padding[1];
+  U16 count;
+  S32 encodings[0]; // 'count' entries.
+} PACKED SetEncodings_t;
+
+BUILD_BUG_ON(sizeof(SetEncodings_t) != 3);
+
+
+typedef struct {
+  U8 incremental;
+  U16 x;
+  U16 y;
+  U16 w;
+  U16 h;
+} PACKED FramebufferUpdateRequest_t;
+
+typedef struct {
+  U8 down;
+  U8 _padding[2];
+  U32 key;
+} PACKED KeyEvent_t;
+
+typedef struct {
+  U8 button_mask;
+  U16 x;
+  U16 y;
+} PACKED PointerEvent_t;
+
+// Variable length:
+typedef struct {
+  U8 _padding[3];
+  U32 len;
+  char text[0]; // 'len' bytes.
+} PACKED ClientCutText_t;
+
 
 typedef struct {
   U8 width[2];
@@ -51,10 +124,10 @@ typedef struct {
   pixel_format format;
   U8 name_length[4];
   U8 name[0];
-} server_init;
+} PACKED server_init;
 
 
-int Send(int sock, char* data, int len, int flags)
+int Send(int sock, char *data, int len, int flags)
 {
   int i;
   for (i=0; i<len; ++i) { printf("%02X ",(unsigned char)data[i]); }
@@ -94,7 +167,7 @@ int SOCK_SendU8(int sock, unsigned int value)
 {
   unsigned char buffer[1];
   buffer[0] = value & 0xFFL;
-  return Send(sock, buffer, sizeof(buffer), 0);
+  return Send(sock, (char*)buffer, sizeof(buffer), 0);
 }
 
 
@@ -103,7 +176,7 @@ int SOCK_SendU16(int sock, unsigned int value)
   unsigned char buffer[2];
   buffer[1] = value & 0xFFL; value >>= 8;
   buffer[0] = value & 0xFFL;
-  return Send(sock, buffer, sizeof(buffer), 0);
+  return Send(sock, (char*)buffer, sizeof(buffer), 0);
 }
 
 
@@ -114,7 +187,7 @@ int SOCK_SendU32(int sock, unsigned int value)
   buffer[2] = value & 0xFFL; value >>= 8;
   buffer[1] = value & 0xFFL; value >>= 8;
   buffer[0] = value & 0xFFL;
-  return Send(sock, buffer, sizeof(buffer), 0);
+  return Send(sock, (char*)buffer, sizeof(buffer), 0);
 }
 
 
@@ -232,6 +305,9 @@ char *RFB_WaitFor(rfb_conn *pc, int bytes)
 }
 
 
+#define RFB_WaitForStruct(zzconn,zzstruct)  ((zzstruct *)RFB_WaitFor((zzconn), sizeof(zzstruct)))
+
+
 int RFB_WaitForU8(rfb_conn *pc, unsigned int *value)
 {
   char* data = RFB_WaitFor(pc, 1);
@@ -328,6 +404,77 @@ int RFB_FramebufferUpdate(rfb_conn *pc)
 }
 
 
+enum {
+  STATE_HANDSHAKE,
+  STATE_READY,
+};
+
+
+enum {
+  kSetPixelFormat = 0,
+  kSetEncodings = 2,
+  kFramebufferUpdateRequest = 3,
+  kKeyEvent = 4,
+  kPointerEvent = 5,
+  kClientCutText = 6,
+};
+
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
+int nprint(char *prefix, char *str, int len, char *suffix)
+{
+  char *buffer = calloc(len+1, 0);
+  if (!buffer)
+  {
+    return -1;
+  }
+  memcpy(buffer, str, len);
+  int result = printf("%s%s%s", Default(prefix,""), buffer, Default(suffix,""));
+  free(buffer);
+  return result;
+}
+
+
+void DEBUG_HexDump(char *data, int len, char *heading)
+{
+  int i, j;
+  char c;
+  char cbuf[19];
+  if (heading) printf("%s\n", heading);
+  // Go line by line: 16 bytes at a time:
+  for (i=0; i<len; i+=16)
+  {
+    printf("%04x: ", i);
+    strcpy(cbuf, "|                |");
+    // Process each byte out of the 16:
+    for (j=0; j<16 && (i+j)<len; ++j)
+    {
+      c = data[i+j];
+      printf("%02x%c", (unsigned char)c, (j==7) ? '-' : ' ');
+      cbuf[1+j] = isprint(c) ? c : '.';
+    }
+    // Pad out, and print trailing ASCII representation:
+    while (j++ < 16) printf("   ");
+    printf(" %s\n", cbuf);
+  }
+}
+
+
+#define HEXDUMP(zzhead,zzsrc,zzmul,zzextra) DEBUG_HexDump((char*)(zzsrc),sizeof(*(zzsrc))*(zzmul)+(zzextra),(zzhead))
+
+
+#define CASE_PRINT(zzconst) case zzconst: { printf("%s", (#zzconst)); break; }
+
 
 void RFB_HandleClient(int sock)
 {
@@ -342,32 +489,198 @@ void RFB_HandleClient(int sock)
     printf("RFB_OpenClient failed\n");
     return;
   }
-  do { // Dummy loop to make 'break' easier.
-    if (NULL == (tmp = RFB_WaitFor(&conn, 12)))
+  int state = STATE_HANDSHAKE;
+  int abort = 0;
+
+  // RFB message loop:
+  while (!abort)
+  {
+    switch (state)
     {
-      printf("Didn't get client version string\n");
-      break;
+      case STATE_HANDSHAKE:
+      {
+        // Expecting version string from client, then ClientInit (share flag)...
+        tmp = RFB_WaitFor(&conn, 12);
+        if (!tmp)
+        {
+          printf("Didn't get client version string\n");
+          abort = 1;
+          break;
+        }
+        nprint("Client version: ", tmp, 12, "\n");
+        // Send our required security type:
+        SOCK_SendU32(sock, RFB_SEC_NONE);
+        // Expect ClientInit (share flag byte):
+        if (RFB_WaitForU8(&conn, &value) < 0)
+        {
+          printf("Didn't get ClientInit\n");
+          abort = 1;
+          break;
+        }
+        printf("ClientInit share flag: %d\n", value);
+        // Send ServerInit:
+        if (RFB_ServerInit(&conn, 500, 500, "Anton's Test Server") < 0)
+        {
+          printf("ServerInit failed\n");
+          abort = 1;
+          break;
+        }
+        printf("ServerInit sent; ready for Client commands\n");
+        state = STATE_READY;
+        break;
+      }
+      case STATE_READY:
+      {
+        // Wait for command [byte] from client:
+        if (RFB_WaitForU8(&conn, &value) < 0)
+        {
+          printf("Failed while waiting for client command: Disconnected?\n");
+          abort = 1;
+          break;
+        }
+        printf("Client command: ");
+        switch (value)
+        {
+          case kSetPixelFormat:
+          {
+            SetPixelFormat_t *m;
+            printf("SetPixelFormat");
+            m = RFB_WaitForStruct(&conn, SetPixelFormat_t);
+            if (!m)
+            {
+              printf(" - Failed!");
+              abort = 1;
+              break;
+            }
+            #warning TODO: Handle SetPixelFormat!
+            printf(" - Not implemented\n");
+            HEXDUMP("", m, 1, 0);
+            break;
+          }
+          case kSetEncodings:
+          {
+            SetEncodings_t *m;
+            int count;
+            S32 *encoding_types;
+            printf("SetEncodings");
+            m = RFB_WaitForStruct(&conn, SetEncodings_t);
+            if (!m)
+            {
+              printf(" - Failed!");
+              abort = 1;
+              break;
+            }
+            printf("\n");
+            HEXDUMP("", m, 1, 0);
+            count = RFB16(m->count);
+            if (count > 0)
+            {
+              // Get extra data:
+              printf(" x %d", count);
+              encoding_types = (S32*)RFB_WaitFor(&conn, sizeof(S32)*count);
+              if (!encoding_types)
+              {
+                printf(" - Failed getting %d encoding types!", count);
+                abort = 1;
+                break;
+              }
+            }
+            #warning TODO: Handle SetEncodings!
+            printf(" - Not implemented\n");
+            HEXDUMP("", encoding_types, count, 0);
+            break;
+          }
+          case kFramebufferUpdateRequest:
+          {
+            FramebufferUpdateRequest_t *m;
+            printf("FramebufferUpdateRequest");
+            m = RFB_WaitForStruct(&conn, FramebufferUpdateRequest_t);
+            if (!m)
+            {
+              printf(" - Failed!");
+              abort = 1;
+              break;
+            }
+            #warning TODO: Handle FramebufferUpdateRequest!
+            printf(" - Not implemented\n");
+            HEXDUMP("", m, 1, 0);
+            break;
+          }
+          case kKeyEvent:
+          {
+            KeyEvent_t *m;
+            printf("KeyEvent");
+            m = RFB_WaitForStruct(&conn, KeyEvent_t);
+            if (!m)
+            {
+              printf(" - Failed!");
+              abort = 1;
+              break;
+            }
+            #warning TODO: Handle KeyEvent!
+            printf(" - Not implemented\n");
+            printf("Key '%c' %s", RFB32(m->key), m->down ? "down" : "up");
+            // HEXDUMP("", m, 1, 0);
+            break;
+          }
+          case kPointerEvent:
+          {
+            PointerEvent_t *m;
+            printf("PointerEvent");
+            m = RFB_WaitForStruct(&conn, PointerEvent_t);
+            if (!m)
+            {
+              printf(" - Failed!");
+              abort = 1;
+              break;
+            }
+            #warning TODO: Handle PointerEvent!
+            printf(" - Not implemented\n");
+            printf("Pos: (%d,%d) - Buttons: "BYTE_TO_BINARY_PATTERN, RFB16(m->x), RFB16(m->y), BYTE_TO_BINARY(m->button_mask));
+            // HEXDUMP("", m, 1, 0);
+            break;
+          }
+          case kClientCutText:
+          {
+            ClientCutText_t *m;
+            int len;
+            char *text;
+            printf("ClientCutText");
+            m = RFB_WaitForStruct(&conn, ClientCutText_t);
+            if (!m)
+            {
+              printf(" - Failed!");
+              abort = 1;
+              break;
+            }
+            len = m->len;
+            if (len > 0)
+            {
+              // Get extra data:
+              text = RFB_WaitFor(&conn, sizeof(U8)*len);
+              if (!text)
+              {
+                printf(" - Failed getting %d bytes!", len);
+                abort = 1;
+                break;
+              }
+              printf(" x %d byte(s)", len);
+            }
+            #warning TODO: Handle ClientCutText!
+            printf(" - Not implemented");
+            break;
+          }
+          default:
+          {
+            printf("Unknown (0x%02X)", (U8)value);
+            break;
+          }
+        }
+        printf("\n");
+        break;
+      }
     }
-    memcpy(ver_string, tmp, 12);
-    printf("Client version: %s", ver_string);
-    // Send required security type:
-    SOCK_SendU32(sock, RFB_SEC_NONE);
-    // Wait for client to send init:
-    if (RFB_WaitForU8(&conn, &value) < 0)
-    {
-      printf("Didn't get ClientInit\n");
-      break;
-    }
-    printf("ClientInit shared flag: %d\n", value);
-    RFB_ServerInit(&conn, 640, 480, "Anton's Test Server");
-    RFB_FramebufferUpdate(&conn);
-    sleep(1);
-    RFB_FramebufferUpdate(&conn);
-    sleep(1);
-    RFB_FramebufferUpdate(&conn);
-    printf("Wrapping up...\n");
-    sleep(3);
-  } while (0);
+  }
   printf("Closing connection %d\n", sock);
   RFB_CloseClient(&conn);
 }
@@ -375,21 +688,46 @@ void RFB_HandleClient(int sock)
 
 int main(int argc, char **argv)
 {
+  int result;
   int server_socket, client_socket;
   struct sockaddr_in server_host, client_host;
+
   server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (server_socket < 0)
+  {
+    printf("Failed to create server socket. Result: %d\n", server_socket);
+    exit(1);
+  }
   memset(&server_host, 0, sizeof(server_host));
   server_host.sin_family = AF_INET;
   server_host.sin_addr.s_addr = htonl(INADDR_ANY);
   server_host.sin_port = htons(PORT);
-  bind(server_socket, (struct sockaddr*)&server_host, sizeof(server_host));
-  listen(server_socket, MAXPENDING);  // Last arg is our connection queue limit.
+  result = bind(server_socket, (struct sockaddr*)&server_host, sizeof(server_host));
+  if (result < 0)
+  {
+    printf("Failed to bind to socket %d. Result: %d\n", server_socket, result);
+    close(server_socket);
+    exit(1);
+  }
+  result = listen(server_socket, MAXPENDING);  // Last arg is our connection queue limit.
+  if (result < 0)
+  {
+    printf("Failed to listen to socket %d. Result: %d\n", server_socket, result);
+    close(server_socket);
+    exit(1);
+  }
   while (1)
   {
     unsigned int client_host_len = sizeof(client_host);
     // Block, waiting to accept a new connection:
     printf("Awaiting connection...\n");
     client_socket = accept(server_socket, (struct sockaddr*)&client_host, &client_host_len);
+    if (client_socket < 0)
+    {
+      printf("Failed to accept client on socket %d. Result: %d\n", server_socket, client_socket);
+      close(server_socket);
+      exit(1);
+    }
     RFB_HandleClient(client_socket);
   }
 }
